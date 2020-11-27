053af89d1506e744c960e494654856af30ba0b1d
root <sensm608@gmail.com>
2020-11-27 21:41:06 +0900

    down2

---
+++ countmain.go
@@ -0,0 +1,37 @@
+package main
+
+import ("fmt"
+	"log"
+	"../datafile"
+	)
+func main(){
+	lines, err := datafile.GetStrings("votes.txt")
+	if err != nil{
+		log.Fatal(err)
+	}
+	//var names []string
+	//var counts []int
+	//for _, line := range lines{
+	//	matched := false
+	//	for i, name := range names{
+	//		if name == line{
+	//			counts[i]++
+	//			matched = true
+	//		}
+	//	}
+	//	if matched == false{
+	//		names = append(names, line)
+	//		counts = append(counts, 1)
+	//	}
+	//}
+	//for i, name := range names{
+	//	fmt.Printf("%s: %d\n", name, counts[i])
+	//}
+	counts := make(map[string]int)
+	for _, line := range lines{
+		counts[line]++
+	}
+	for key, val := range counts{
+		fmt.Printf("%s: %d\n", key,val)
+	}
+}
