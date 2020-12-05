e1aa868afdb46ff9e058a6204ba6114c7262971b
root <root@DESKTOP-5BARE4F.localdomain>
2020-12-05 21:32:30 +0900

    14week lec

---
+++ go_goroutine_01.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+import	"time"
+
+func x() {
+	for i := 0; i < 10; i++ {
+		fmt.Print("x")
+	}
+}
+
+func y() {
+	for i := 0; i < 10; i++ {
+		fmt.Print("y")
+	}
+}
+
+func z() {
+	fmt.Println("z")
+}
+
+func k(){
+	fmt.Println("k")
+}
+
+func main() {
+	defer z()
+	defer k()
+	go x()
+	go y()
+	time.Sleep(time.Second * 2)
+	fmt.Println("end main()")
+}
